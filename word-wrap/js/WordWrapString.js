c66a23740ce61de6154157a2729b905d3d0c3ec4
Fernando Daciuk <f.daciuk@gmail.com>
2014-08-27 02:10:14 +0000

    Word Wrap string - pass first test

--- word-wrap/js/WordWrapString.js
+++ word-wrap/js/WordWrapString.js
@@ -19,6 +19,12 @@
 
         // ------------------------
 
+        $public.wrap = function wrap( text, maxLength ) {
+            return 'string';
+        }
+
+        // ------------------------
+
         return $public;
     };
 
