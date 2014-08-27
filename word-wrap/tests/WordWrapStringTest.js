2141af1e12e7500b52fdf7cd35ed92a24b948961
Fernando Daciuk <f.daciuk@gmail.com>
2014-08-27 02:12:48 +0000

    Add second test to Word Wrap String

--- word-wrap/tests/WordWrapStringTest.js
+++ word-wrap/tests/WordWrapStringTest.js
@@ -6,8 +6,13 @@ describe( 'WordWrapString.js', function() {
         ( true ).should.be.ok;
     });
 
-    it( 'should return the own text on the first param', function() {
+    it( 'should return the text passed on the first param', function() {
         var wordwrap = WordWrap.wrap( 'string' );
         wordwrap.should.be.equal( 'string' );
+    });
+
+    it( 'should return the first param when the second param is null or undefined', function() {
+        var wordwrap = WordWrap.wrap( 'my string' );
+        wordwrap.should.be.equal( 'my string' );
     });
 });
