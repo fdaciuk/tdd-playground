c56b960447d72b8685e86deb9297a0210ffaa39f
Fernando Daciuk <f.daciuk@gmail.com>
2014-08-29 02:56:47 +0000

    Fix travis CI and add gulp

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require( 'gulp' );
+var mocha = require( 'gulp-mocha' );
+
+gulp.task( 'mocha', function() {
+    gulp.src( 'word-wrap/tests/*.js', { read : false })
+    .pipe(
+        mocha(
+            { reporter: 'list' }
+        )
+    );
+});
+
+gulp.task( 'default', function() {
+    gulp.watch( 'word-wrap/tests/*.js', [ 'mocha' ] );
+});
